ceb7f566b187975e2e573cf6aee6be1894207d89
logunkov <logunkovkn@gmail.com>
2023-12-13 19:44:16 +0700

    Upgrade Theme

--- App/Gismeteo/Theme/Sizes.swift
+++ App/Gismeteo/Theme/Sizes.swift
@@ -10,21 +10,39 @@ import UIKit
 // MARK: - Sizes
 
 enum Sizes {
-
 	// MARK: - Medium
 
 	enum Medium {
 		static let halfPadding: CGFloat = 8
 		static let padding: CGFloat = 16
+		static let halfCornerRadius: CGFloat = 8
 		static let cornerRadius: CGFloat = 16
+	}
+
+	// MARK: - Font
+
+	enum Font {
+		static let headerView: CGFloat = 90
+		static let action: CGFloat = 16
+	}
+
+	// MARK: - Collection
+
+	enum Collection {
+		static let hoursDay = 24
+		static let widthtMultiplier: CGFloat = 0.7
+		static let heightMultiplier: CGFloat = 0.9
 	}
 
 	// MARK: - Table
 
 	enum Table {
 		static let contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
-		static let week: CGFloat = 7
 		static let spacing: CGFloat = 1
+		static let section = 1
+		static let heightsection: CGFloat = 100
+		static let sectionHeaderHeight: CGFloat = 4
+		static let sectionFooterHeight: CGFloat = 4
 	}
 
 	// MARK: - Gradient
@@ -45,5 +63,23 @@ enum Sizes {
 
 	enum CarouselView {
 		static let screenMultiplier: CGFloat = 0.9
+	}
+
+	// MARK: - Coord
+
+	enum Coord {
+		static let uncertainty: Double = 100
+	}
+
+	// MARK: - Screen
+
+	enum Screen {
+		static let half: CGFloat = 2
+		static let week: CGFloat = 7
+		static let heightCell: CGFloat = half * week
+		static let collectionView: CGFloat = 8
+		static let iconImageView: CGFloat = 0.35
+		static let highTempLabel: CGFloat = 0.5
+		static let lowTempLabel: CGFloat = 0.75
 	}
 }
--- App/Gismeteo/Theme/Theme.swift
+++ App/Gismeteo/Theme/Theme.swift
@@ -10,18 +10,18 @@ import UIKit
 // MARK: - Theme
 
 enum Theme {
-
 	// MARK: - Internal properties
 
 	static let backgroundColor = UIColor.systemBlue
 	static let white = UIColor.white
 	static let black = UIColor.black
 	static let gray = UIColor.systemGray
+	static let red = UIColor.systemRed
 
 	// MARK: - Gradient
 
-	enum  Gradient {
-		static let startColor = UIColor.systemBlue.withAlphaComponent(0.5)
-		static let endColor = UIColor.systemBlue.withAlphaComponent(0.9)
+	enum Gradient {
+		static let startColor = UIColor.white.withAlphaComponent(0.5)
+		static let endColor = UIColor.white.withAlphaComponent(0.1)
 	}
 }
