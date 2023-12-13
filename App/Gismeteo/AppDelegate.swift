7bdee2b901e2ad3f75b67a35e53afdc1482c63de
logunkov <logunkovkn@gmail.com>
2023-12-13 19:46:11 +0700

    Upgrade App

--- App/Gismeteo/AppDelegate.swift
+++ App/Gismeteo/AppDelegate.swift
@@ -5,7 +5,53 @@
 //  Created by Constantin on 22.11.2023.
 //
 
+import CoreLocation
 import UIKit
 
+// MARK: - AppDelegate
+
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {}
+
+final class AppDelegate: UIResponder, UIApplicationDelegate { private let locationManager = CLLocationManager()
+
+	// MARK: - Internal methods
+
+	func application(
+		_: UIApplication,
+		didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+	) -> Bool {
+		setupLocationManager()
+		setupRepository()
+
+		return true
+	}
+
+	// MARK: - Private methods
+
+	private func setupLocationManager() {
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyBest
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.startUpdatingLocation()
+	}
+
+	private func setupRepository() {
+		Repository.shared.loadBackup()
+	}
+}
+
+// MARK: CLLocationManagerDelegate
+
+extension AppDelegate: CLLocationManagerDelegate {
+	// MARK: - Internal methods
+
+	func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		guard let location = locations.last else { return }
+		locationManager.stopUpdatingLocation()
+		let latitude = location.coordinate.latitude
+		let longitude = location.coordinate.longitude
+		Repository.shared.setLocation(latitude: latitude, longitude: longitude)
+	}
+
+	func locationManager(_: CLLocationManager, didFailWithError _: Error) {}
+}
--- App/Gismeteo/SceneDelegate.swift
+++ App/Gismeteo/SceneDelegate.swift
@@ -7,20 +7,31 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+// MARK: - SceneDelegate
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	// MARK: - Internal properties
+
 	var window: UIWindow?
+
+	// MARK: - Internal methods
 
 	func scene(
 		_ scene: UIScene,
 		willConnectTo _: UISceneSession,
 		options _: UIScene.ConnectionOptions
 	) {
-		// Проверяем, что сцена является окном.
 		guard let winScene = (scene as? UIWindowScene) else { return }
 
-		// Устанавливаем корневой контроллер.
 		let window = UIWindow(windowScene: winScene)
-		window.rootViewController = UINavigationController(rootViewController: ViewController())
+		window.rootViewController = UINavigationController(rootViewController: MainAssembler().assembly(isMain: true))
+
+		#if DEBUG
+			if ProcessInfo.processInfo.arguments.contains("-launchWithFindScene") {
+				window.rootViewController = UINavigationController(rootViewController: FindAssembler().assembly(weatherModel: []))
+			}
+		#endif
+
 		window.makeKeyAndVisible()
 		self.window = window
 	}
